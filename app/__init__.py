3a9082d7e240db58fa6d3e24d770d181ddb3dafb
KOSASIH <kosasihg88@gmail.com>
2024-06-17 17:02:54 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+
+from app.controllers import block_controller, transaction_controller
+from app.views import block_view, transaction_view
